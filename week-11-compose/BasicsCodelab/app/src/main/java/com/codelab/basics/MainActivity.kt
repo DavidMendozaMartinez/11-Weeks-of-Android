f97243c9eacc0d13ba7b4bb8864859ea6569a69d
David Mendoza <david.mendoza.mnez@gmail.com>
2020-11-29 16:59:01 +0100

    Week 11 - Jetpack Compose: Add two Texts and a Button to MainActivity. Set the background color to yellow. When the button is clicked it updates the counter of times it has been clicked and its background color

--- week-11-compose/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
+++ week-11-compose/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
@@ -2,11 +2,17 @@ package com.codelab.basics
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.Text
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.setContent
+import androidx.compose.ui.unit.dp
 import androidx.ui.tooling.preview.Preview
 import com.codelab.basics.ui.BasicsCodelabTheme
 
@@ -14,25 +20,63 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            BasicsCodelabTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+            MyApp {
+                MyScreenContent()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MyApp(content: @Composable () -> Unit) {
+    BasicsCodelabTheme {
+        Surface(color = Color.Yellow) {
+            content()
+        }
+    }
 }
 
-@Preview(showBackground = true)
+@Composable
+fun MyScreenContent(names: List<String> = listOf("Android", "there")) {
+    val counterState = remember { mutableStateOf(0) }
+
+    Column(modifier = Modifier.fillMaxHeight()) {
+        Column(modifier = Modifier.weight(1f)) {
+            for (name in names) {
+                Greeting(name = name)
+                Divider(color = Color.Black)
+            }
+        }
+        Counter(
+                count = counterState.value,
+                updateCount = { newCount ->
+                    counterState.value = newCount
+                }
+        )
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!", modifier = Modifier.padding(24.dp))
+}
+
+@Composable
+fun Counter(count: Int, updateCount: (Int) -> Unit) {
+    Button(
+            onClick = { updateCount(count + 1) },
+            colors = ButtonConstants.defaultButtonColors(
+                    backgroundColor = if (count > 5) Color.Green else Color.White
+            )
+    ) {
+        Text("I've been clicked $count times")
+    }
+}
+
+@Preview("MyScreen preview")
 @Composable
 fun DefaultPreview() {
-    BasicsCodelabTheme {
-        Greeting("Android")
+    MyApp {
+        MyScreenContent()
     }
 }
